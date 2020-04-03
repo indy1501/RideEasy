8439b2760cab2b82123ea6434e02168ff27a7439
indy1501 <indy.isb@gmail.com>
2020-04-03 14:27:38 -0700

    add routes for admin and user

---
+++ routes/admin.routes.js
@@ -0,0 +1,14 @@
+module.exports = app => {
+    const admin = require("../controllers/admin.controller.js");
+
+    //all the routes related to user to be updated in this file
+  
+    // Add vehicle
+
+    // Update vehicle condition
+  
+    // Update late fees
+   
+    // Delete membership
+  
+  };
