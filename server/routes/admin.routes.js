d28ef8ec9c1d27b874b61efbe51eee0ba171be3c
indy1501 <indy.isb@gmail.com>
2020-04-08 14:15:01 -0700

    API to add vehicle to the system and retrieve unreserved vehicles by certain search criteria

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     const admin = require("../controllers/admin.controller.js");
 
-    //all the routes related to user to be updated in this file
+    //all the routes related to admin to be updated in this file
   
     // Add vehicle
 
