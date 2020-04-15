0c70be0196112aa1ac024d94df2c1d32c3ecae50
jyothihr92 <fnu.jyothihassanramesh@sjsu.edu>
2020-04-15 16:56:35 -0700

    Fetching all the details of vehicle_price_range table for the admin

--- server/routes/vehicle_price_range.routes.js
+++ server/routes/vehicle_price_range.routes.js
@@ -1,8 +1,11 @@
 module.exports = app => {
     const Pricerange = require("../controllers/vehicle_price_range.controller.js");
   
-    // Create a new reservation
+    // route for creating records in vehicle_price_range table
     app.post("/vehiclepricerange", Pricerange.create);
     console.log("here in routes")
+
+    //route for fetching all the recprds from the vehicle_price_range table
+    app.get("/vehiclepricerange", Pricerange.findAll);
 
 };
