0357a36736e6aa921226cf4fc8796109cf732546
indy1501 <indy.isb@gmail.com>
2020-04-17 09:08:22 -0700

    Implement API to fetch membershipstatus of a user by User UUID
    Implement API to fetch membership status of a user by User UUID
    
    Implement API to fetch membership status of a user by User UUID

--- server/routes/membership.routes.js
+++ server/routes/membership.routes.js
@@ -10,6 +10,9 @@ module.exports = app => {
 // Retrieve a specific member with membershipId
   app.get("/membership/:membershipUuid", membership.findByUuid);
 
+// Retrieve membership status of a user by User UUID
+  app.get("/membership/:userUuid", membership.findByUserUuid);
+
 //update membership status when admin deletes the membership
   app.patch("/membership/:membershipUuid", membership.updateOne);
 
