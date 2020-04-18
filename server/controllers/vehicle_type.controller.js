041dc8de86b47ee3f9a5304207f8b4f155a16c32
mayurib2 <bhisemayuri@gmail.com>
2020-04-18 16:49:54 -0700

    Implement API to fetch all vehicle types

--- server/controllers/vehicle_type.controller.js
+++ server/controllers/vehicle_type.controller.js
@@ -1,6 +1,19 @@
+
+
 const VehicleType = require("../models/vehicle_type.model.js");
 const { uuid } = require('uuidv4');
 
+//Get all vehicle types
+exports.getAll = (req,res) => {
+    VehicleType.getAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Error occurred while retrieving locations."
+            });
+        res.send(data);
+    });
+}
 // Add Vehicle Type
 exports.create = (req, res) => {
     // Validate request
