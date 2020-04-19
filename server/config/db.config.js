a4845d21744abb7ba10f127dc53e4863f5661bf2
mayurib2 <bhisemayuri@gmail.com>
2020-04-18 20:57:59 -0700

    Update lambda to use subject uuid inside IDToken as user uuid

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root1234!",
-  DB: "rental_car_system",
-}
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "Secret@11",
+    DB: "rental_car_system"
+  };
