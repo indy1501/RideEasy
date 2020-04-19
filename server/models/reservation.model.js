78e26d1a5aa8264d398324ba1d19b86d08de5b14
jyothihr92 <fnu.jyothihassanramesh@sjsu.edu>
2020-04-19 02:19:14 -0700

    Cancel reservation API completed

--- server/models/reservation.model.js
+++ server/models/reservation.model.js
@@ -48,6 +48,7 @@ reservation.create = (newReservation, result) => {
 //is_reserved field to false in vehicle table
 reservation.removebyUuid = (uuid, result) => {
   var vehicle_uuid;
+  var cancellation_fee;
   var sql_qry_string = "select vehicle_uuid from reservation where uuid = \'"+escape(uuid)+"'";
   console.log(sql_qry_string);
   sql.query(sql_qry_string,(err, res) => {
@@ -66,18 +67,59 @@ reservation.removebyUuid = (uuid, result) => {
     sql.query(qry_str, uuid, (err, res) => {
     if (err) {
       console.log("error: ", err);
-      console.log("user id",uuid);
+      console.log("reservation id",uuid);
       result(null, err);
-      //if cancellation of reservation is not successful ,then we need to charge one hour price to user
-      //var querystring = "select "
+      
       return;
+
     }
 
     if (res.affectedRows == 0) {
       // not found reservation with the id
-      result({ kind: "not_found" }, null);
+      //result({ kind: "not_found" }, null);
+      console.log("second case");
+      //if cancellation of reservation is not successful ,then we need to charge one hour price to user
+      var querystring = "select vt.price as cancellation_fee,vt.vehicle_type_uuid as vehicletype from vehicle_price_range vt join vehicle v on v.vehicle_type_uuid = vt.vehicle_type_uuid join reservation r on r.vehicle_uuid = v.uuid where (vt.min_hours=1 and vt.max_hours=5 ) and r.uuid = \'"+escape(uuid)+"'";
+      console.log("query string" + querystring);
+      sql.query(querystring,(err, res) => {
+        if(err){
+          console.log("error: ", err);
+          
+          return;
+        }
+        console.log("cancellation fee calculated");
+        console.log("cancellation fee =" +res[0].cancellation_fee);
+        //console.log("vehicle_type_uuid" + res[1].vehicletype);
+        cancellation_fee = res[0].cancellation_fee;
+        var query ="DELETE  FROM reservation WHERE uuid = \'"+escape(uuid)+"'";
+        sql.query(query,(err, res) => {
+          if(err){
+            console.log("error: ", err);
+            
+            return;
+          }
+          console.log("reservation got cancelled with cancellation fee =" +cancellation_fee);
+          console.log("res"+res);
+          result(null,res);
+          var qrystring = "UPDATE vehicle SET is_reserved = false WHERE uuid = \'"+ escape(vehicle_uuid) + "'";
+          console.log("querystring = " + qrystring);
+          sql.query(qrystring,(err, res) => {
+          if(err){
+          console.log("error: ", err);
+        
+          return;
+      }
+         
+      console.log("vehicle table updated");
+     
+      
+    });
+    
+  });
+
+});
       return;
-    }
+  }
 
     console.log("deleted reservation record with reservation id: ", uuid);
     result(null, res);
@@ -91,7 +133,8 @@ reservation.removebyUuid = (uuid, result) => {
         return;
       }
       console.log("vehicle table updated");
-
+     
+      
     }
    );
   });
