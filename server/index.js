13b2c0585b03a965479dd1fa5c9bb72ecb05e330
mayurib2 <bhisemayuri@gmail.com>
2020-05-01 00:31:24 -0700

    Implement API to create policy

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ require("./routes/vehicle_price_range.routes.js")(app);
 require("./routes/location.routes.js")(app);
 require("./routes/users.routes.js")(app);
 require("./routes/vehicle_type.routes.js")(app);
+require("./routes/policy.routes.js")(app);
 
 
 app.listen(server, () => {
