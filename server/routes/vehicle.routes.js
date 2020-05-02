7d57e0141cbe4b129d1def932a0539e2ab84255c
indy1501 <indy.isb@gmail.com>
2020-05-02 13:34:08 -0700

    route changes for vehicle search api

--- server/routes/vehicle.routes.js
+++ server/routes/vehicle.routes.js
@@ -1,3 +1,4 @@
+
 module.exports = (app) => {
   const vehicles = require("../controllers/vehicle.controller.js")
 
@@ -5,14 +6,10 @@ module.exports = (app) => {
   app.post("/vehicles", vehicles.create)
 
   // Retrieve all unreserved Vehicles which satisfy the search criteria
-  app.get(
-    "/vehicles?:vehicle_type?:location?:reservation_start_time?:reservation_end_time",
-    vehicles.findBySearchCriteria
-  )
 
   // Retrieve a single vehicle with vehicle UUID
   app.get("/vehicles/:vehicleUuid", vehicles.findVehicleByUuid)
 
    //For Admin to retrieve all vehicles in the system
-   app.get("/vehicles", vehicles.findAllVehicles)
+   app.get("/vehicles", vehicles.findVehicles)
 }
