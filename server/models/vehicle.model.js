a5d4e120fe3716e836fdea48d518e85e73c3251e
indy1501 <indy.isb@gmail.com>
2020-05-01 20:01:26 -0700

    Updated Vehicle search criteria to show available vehicles to the user

--- server/models/vehicle.model.js
+++ server/models/vehicle.model.js
@@ -29,13 +29,26 @@ Vehicle.create = (newVehicle, result) => {
   })
 }
 
-Vehicle.getAll = (vehicleType, city, startTime, result) => {
+
+// Retrieve all unreserved Vehicles which satisfy the search criteria
+
+Vehicle.getBySearchCriteria = (
+  vehicle_type,
+  location,
+  reservation_start_time,
+  reservation_end_time,
+  result
+) => {
   sql.query(
-    `SELECT T.uuid, L.uuid, V.uuid, V.vehicle_type_uuid, V.model, V.make, V.year, V.registration_number, V.current_mileage, V.last_serviced_date, V.is_reserved, V.vehicle_condition, V.next_available_time, V.location_uuid, T.type, L.name, L.address, L.city, L.state, L.zip_code, L.capacity, L.number_of_vehicles FROM vehicle V LEFT OUTER JOIN vehicle_type T ON V.vehicle_type_uuid = T.uuid LEFT OUTER JOIN location L ON V.location_uuid = L.uuid WHERE T.type = \'${escape(
-      vehicleType
-    )}\' AND L.city = \'${escape(city)}\' AND V.next_available_time >= \'${escape(
-      startTime
-    )}\'`,
+    `SELECT T.uuid, L.uuid, R.uuid, V.uuid, V.vehicle_type_uuid, V.model, V.make, V.year, V.registration_number, V.current_mileage, V.last_serviced_date, V.vehicle_condition, V.next_available_time, V.location_uuid, T.type, L.name, L.address, L.city, L.state, L.zip_code, L.capacity, L.number_of_vehicles, R.vehicle_uuid, R.user_uuid, R.start_date, R.end_date, R.is_pickedUp FROM vehicle V LEFT OUTER JOIN vehicle_type T ON V.vehicle_type_uuid = T.uuid LEFT OUTER JOIN location L ON V.location_uuid = L.uuid LEFT OUTER JOIN reservation R ON V.uuid = R.vehicle_uuid WHERE R.is_pickedUp = false AND T.type = \'${escape(
+      vehicle_type
+    )}\' AND L.city = \'${escape(location)}\' AND (((\'${escape(
+      reservation_start_time
+    )}\' < R.start_date) AND (\'${escape(
+      reservation_end_time
+    )}\' < R.start_date )) OR ((\'${escape(
+      reservation_start_time
+    )}\' > R.end_date) AND (\'${escape(reservation_start_time)}\' > R.end_date)))`,
     (err, res) => {
       if (err) {
         console.log("error: ", err)
@@ -49,9 +62,17 @@ Vehicle.getAll = (vehicleType, city, startTime, result) => {
         return
       } else {
         sql.query(
-          `SELECT T.uuid, L.uuid, V.uuid, V.vehicle_type_uuid, V.model, V.make, V.year, V.registration_number, V.current_mileage, V.last_serviced_date, V.is_reserved, V.vehicle_condition, V.next_available_time, V.location_uuid, T.type, L.name, L.address, L.city, L.state, L.zip_code, L.capacity, L.number_of_vehicles FROM vehicle V LEFT OUTER JOIN vehicle_type T ON V.vehicle_type_uuid = T.uuid LEFT OUTER JOIN location L ON V.location_uuid = L.uuid WHERE V.is_reserved = false AND T.type = \'${escape(
-            vehicleType
-          )}\' AND V.next_available_time >= \'${escape(startTime)}\'`,
+          `SELECT T.uuid, L.uuid, R.uuid, V.uuid, V.vehicle_type_uuid, V.model, V.make, V.year, V.registration_number, V.current_mileage, V.last_serviced_date, V.vehicle_condition, V.next_available_time, V.location_uuid, T.type, L.name, L.address, L.city, L.state, L.zip_code, L.capacity, L.number_of_vehicles, R.vehicle_uuid, R.user_uuid, R.start_date, R.end_date, R.is_pickedUp FROM vehicle V LEFT OUTER JOIN vehicle_type T ON V.vehicle_type_uuid = T.uuid LEFT OUTER JOIN location L ON V.location_uuid = L.uuid LEFT OUTER JOIN reservation R ON V.uuid = R.vehicle_uuid WHERE R.is_pickedUp = false AND T.type = \'${escape(
+            vehicle_type
+          )}\' AND (((\'${escape(
+            reservation_start_time
+          )}\' < R.start_date) AND (\'${escape(
+            reservation_end_time
+          )}\' < R.start_date )) OR ((\'${escape(
+            reservation_start_time
+          )}\' > R.end_date) AND (\'${escape(
+            rreservation_end_time
+          )}\' > R.end_date)))`,
           (err, res) => {
             if (err) {
               console.log("error: ", err)
@@ -69,7 +90,9 @@ Vehicle.getAll = (vehicleType, city, startTime, result) => {
   )
 }
 
-Vehicle.getByUuid = (vehicleUuid, result) => {
+// Retrieve a specific vehicle with vehicle UUID
+
+Vehicle.getVehicleByUuid = (vehicleUuid, result) => {
   sql.query(
     `SELECT * FROM vehicle WHERE uuid = \'${escape(vehicleUuid)}\'`,
     (err, res) => {
@@ -90,7 +113,7 @@ Vehicle.getByUuid = (vehicleUuid, result) => {
   )
 }
 
-//get all vehicles from vehicle table
+//For Admin to retrieve all vehicles in the system
 
 Vehicle.getAllVehicles = (result) => {
   sql.query("SELECT * FROM vehicle", (err, res) => {
